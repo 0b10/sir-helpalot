f32f03e483e968e2262058698a3a323a73f9c340
0b10 <0b10@pm.me>
2019-10-14 03:40:51 +0100

    Create isSafeStrictInteger
      + Test outer boundaries
      + Test inner boundaries
      + Do negative tests for outside of boundary
      + Do positive and fuzz tests
      + Add implementation

--- src/__tests__/unit/predicates/is-strict-integer.test.ts
+++ src/__tests__/unit/predicates/is-strict-integer.test.ts
@@ -2,35 +2,102 @@
 import fc from "fast-check";
 
 import { FAST_CHECK_OPTIONS } from "../../config";
-import { isStrictInteger, isNotStrictInteger } from "../../../predicates";
+import { isSafeStrictInteger, isNotSafeStrictInteger } from "../../../predicates";
 
-describe("isStrictInteger()", () => {
+describe("isSafeStrictInteger()", () => {
   it("should exist", () => {
-    expect(isStrictInteger).toBeDefined();
-    expect(isNotStrictInteger).toBeDefined();
+    expect(isSafeStrictInteger).toBeDefined();
+    expect(isNotSafeStrictInteger).toBeDefined();
   });
 
+  // >>> POSITIVE >>>
   describe("positive", () => {
     it("should return true for random integer values", () => {
       fc.assert(
-        fc.property(fc.integer(), (value) => {
+        fc.property(fc.maxSafeInteger(), (value) => {
           return (
-            isStrictInteger(value) === true && isNotStrictInteger(value) === !isStrictInteger(value)
+            isSafeStrictInteger(value) === true &&
+            isNotSafeStrictInteger(value) === !isSafeStrictInteger(value)
           );
         })
       ),
         FAST_CHECK_OPTIONS;
     });
+
+    it("should return true for values just of the inside upper boundary", () => {
+      const max = Number.MAX_SAFE_INTEGER;
+      for (let val of [max, max - 1, max - 2]) {
+        expect(isSafeStrictInteger(val) && !isNotSafeStrictInteger(val)).toBe(true);
+      }
+    });
+
+    it("should return true for values just inside of the lower boundary", () => {
+      const min = Number.MIN_SAFE_INTEGER;
+      for (let val of [min, min + 1, min + 2]) {
+        expect(isSafeStrictInteger(val) && !isNotSafeStrictInteger(val)).toBe(true);
+      }
+    });
   });
 
+  // >>> NEGATIVE >>>
+  describe("negative", () => {
+    it("should return false for integers > MAX_SAFE_INTEGER", () => {
+      fc.assert(
+        fc.property(
+          fc.integer(Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER + 1000),
+          (value) => {
+            fc.pre(value > Number.MAX_SAFE_INTEGER); // unsafe, so quirky - sits on boundary sometimes
+            return (
+              isSafeStrictInteger(value) === false &&
+              isNotSafeStrictInteger(value) === !isSafeStrictInteger(value)
+            );
+          }
+        )
+      ),
+        FAST_CHECK_OPTIONS;
+    });
+
+    it("should return false for integers < MIN_SAFE_INTEGER", () => {
+      fc.assert(
+        fc.property(
+          fc.integer(Number.MIN_SAFE_INTEGER - 1000, Number.MIN_SAFE_INTEGER - 1),
+          (value) => {
+            fc.pre(value < Number.MIN_SAFE_INTEGER); // unsafe, so quirky - sits on boundary sometimes
+            return (
+              isSafeStrictInteger(value) === false &&
+              isNotSafeStrictInteger(value) === !isSafeStrictInteger(value)
+            );
+          }
+        )
+      ),
+        FAST_CHECK_OPTIONS;
+    });
+
+    // These boundary tests are risky, because they go beyond safe limits. You might need to adapt them
+    it("should return false for values just outside of the upper boundary", () => {
+      const max = Number.MAX_SAFE_INTEGER;
+      for (let val of [max + 1, max + 2, max + 3]) {
+        expect(isSafeStrictInteger(val) && !isNotSafeStrictInteger(val)).toBe(false);
+      }
+    });
+
+    it("should return false for values just outside of the lower boundary", () => {
+      const min = Number.MIN_SAFE_INTEGER;
+      for (let val of [min - 1, min - 2, min - 3]) {
+        expect(isSafeStrictInteger(val) && !isNotSafeStrictInteger(val)).toBe(false);
+      }
+    });
+  });
+
+  // >>> FUZZ >>>
   describe("fuzz", () => {
     it("should return false for random non-integer values", () => {
       fc.assert(
         fc.property(fc.anything(), (value) => {
           fc.pre(!/^-?\d+/.test(value)); // not a digit
           return (
-            isStrictInteger(value) === false &&
-            isNotStrictInteger(value) === !isStrictInteger(value)
+            isSafeStrictInteger(value) === false &&
+            isNotSafeStrictInteger(value) === !isSafeStrictInteger(value)
           );
         })
       ),
--- src/predicates.ts
+++ src/predicates.ts
@@ -14,6 +14,7 @@ export const isNotProductionEnv = () => !isProductionEnv();
  * @returns {bool} true if it is an integer, false otherwise
  */
 export const isStrictInteger = (value: any) => RE_STRICT_INT.test(value);
+
 /**
  * Determine if a value is not a signed integer. There are no lower or upper bound constraints.
  *
@@ -23,3 +24,30 @@ export const isStrictInteger = (value: any) => RE_STRICT_INT.test(value);
  * @returns {bool} true if it is not an integer, false otherwise
  */
 export const isNotStrictInteger = (value: any) => !isStrictInteger(value);
+
+/**
+ * Determine if a value is an integer between MIN_SAFE_INTEGER < n < MAX_SAFE_INTEGER.
+ *
+ * It first checks for a strict integer, then checks boundaries. Non integer types will return false.
+ * Values outside of safe boundaries will return false.
+ *
+ * "Strict" contrasts Number.isInteger(1.0) === true, which is bad.
+ *
+ * @param {any} value = anything
+ * @returns {bool} true if it is a strict integer within the specified boundaries, false otherwise
+ */
+export const isSafeStrictInteger = (value: any) =>
+  isStrictInteger(value) && value >= Number.MIN_SAFE_INTEGER && value <= Number.MAX_SAFE_INTEGER;
+
+/**
+ * Determine if a value is not an integer between MIN_SAFE_INTEGER < n < MAX_SAFE_INTEGER.
+ *
+ * It first checks for a strict integer, then checks boundaries. Non integer types will return true.
+ * Values outside of safe boundaries will return true.
+ *
+ * "Strict" contrasts Number.isInteger(1.0) === true, which is bad.
+ *
+ * @param {any} value = anything
+ * @returns {bool} true if it is not a strict integer within the specified boundaries, false otherwise
+ */
+export const isNotSafeStrictInteger = (value: any) => !isSafeStrictInteger(value);
